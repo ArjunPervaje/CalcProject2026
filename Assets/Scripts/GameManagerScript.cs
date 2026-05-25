65ac1e39e144a6dad2cb7412bae088a3d15bcb99
Arjun Pervaje <arjpervaje@gmail.com>
2026-05-24 22:52:50 -0700

    bug fixes and isWhite rotation logic

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -22,8 +22,14 @@ public class GameManagerScript : MonoBehaviour
     public GameObject blackDeltaPrefab;
     
     private GameObject[,] board;
+    
+    // Camera
+    private GameObject Camera;
 
     private bool acceptingPlayerInput = true;
+
+    public bool isWhite;
+    private Quaternion rotation;
     void Start()
     {
         int rows = 8;
@@ -41,8 +47,19 @@ public class GameManagerScript : MonoBehaviour
 
         /* By Now, the coordinates should all correspond to a square, and you can get the square's
         location by doing board[x, y].transform.position.x or .z for x and z respectfully */
+
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
         
-        
+        if (!isWhite)
+        {
+            rotation = Quaternion.Euler(Quaternion.identity.x, 180, Quaternion.identity.z);
+            Camera.transform.rotation = Quaternion.Euler(90f, 180f, 0f);
+        }
+        else
+        {
+            rotation = Quaternion.identity;
+            Camera.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        }
     }
 
     void Update()
@@ -58,8 +75,13 @@ public class GameManagerScript : MonoBehaviour
 
     public void GameRestart()
     {
+        StartCoroutine(GameRestartRoutine());
+    }
+
+    private IEnumerator GameRestartRoutine()
+    {
         acceptingPlayerInput = false;
-        StartCoroutine(RemoveAllPieces());
+        yield return StartCoroutine(RemoveAllPieces());
         PlaceAllPeices();
         acceptingPlayerInput = true;
     }
@@ -93,23 +115,23 @@ public class GameManagerScript : MonoBehaviour
     private void PlaceAllPeices()
     {
         // White Piece Setup
-        Instantiate(whiteXPrefab, new Vector3(board[0, 0].transform.position.x, 0.5f, board[0, 0].transform.position.z), Quaternion.identity);
-        Instantiate(whiteDeltaPrefab, new Vector3(board[1, 0].transform.position.x, 0.5f, board[1, 0].transform.position.z), Quaternion.identity);
-        Instantiate(whitePiPrefab, new Vector3(board[2, 0].transform.position.x, 0.5f, board[2, 0].transform.position.z), Quaternion.identity);
-        Instantiate(whiteSigmaPrefab, new Vector3(board[3, 0].transform.position.x, 0.5f, board[3, 0].transform.position.z), Quaternion.identity);
-        Instantiate(whiteYPrefab, new Vector3(board[4, 0].transform.position.x, 0.5f, board[4, 0].transform.position.z), Quaternion.identity);
-        Instantiate(whitePiPrefab, new Vector3(board[5, 0].transform.position.x, 0.5f, board[5, 0].transform.position.z), Quaternion.identity);
-        Instantiate(whiteEpsilonPrefab, new Vector3(board[6, 0].transform.position.x, 0.5f, board[6, 0].transform.position.z), Quaternion.identity);
-        Instantiate(whiteXPrefab, new Vector3(board[7, 0].transform.position.x, 0.5f, board[7, 0].transform.position.z), Quaternion.identity);
+        Instantiate(whiteXPrefab, new Vector3(board[0, 0].transform.position.x, 0.5f, board[0, 0].transform.position.z), rotation);
+        Instantiate(whiteDeltaPrefab, new Vector3(board[1, 0].transform.position.x, 0.5f, board[1, 0].transform.position.z), rotation);
+        Instantiate(whitePiPrefab, new Vector3(board[2, 0].transform.position.x, 0.5f, board[2, 0].transform.position.z), rotation);
+        Instantiate(whiteSigmaPrefab, new Vector3(board[3, 0].transform.position.x, 0.5f, board[3, 0].transform.position.z), rotation);
+        Instantiate(whiteYPrefab, new Vector3(board[4, 0].transform.position.x, 0.5f, board[4, 0].transform.position.z), rotation);
+        Instantiate(whitePiPrefab, new Vector3(board[5, 0].transform.position.x, 0.5f, board[5, 0].transform.position.z), rotation);
+        Instantiate(whiteEpsilonPrefab, new Vector3(board[6, 0].transform.position.x, 0.5f, board[6, 0].transform.position.z), rotation);
+        Instantiate(whiteXPrefab, new Vector3(board[7, 0].transform.position.x, 0.5f, board[7, 0].transform.position.z), rotation);
         
         // Black Piece Setup
-        Instantiate(blackXPrefab, new Vector3(board[7, 7].transform.position.x, 0.5f, board[7, 7].transform.position.z), Quaternion.identity);
-        Instantiate(blackDeltaPrefab, new Vector3(board[6, 7].transform.position.x, 0.5f, board[6, 7].transform.position.z), Quaternion.identity);
-        Instantiate(blackPiPrefab, new Vector3(board[5, 7].transform.position.x, 0.5f, board[5, 7].transform.position.z), Quaternion.identity);
-        Instantiate(blackSigmaPrefab, new Vector3(board[4, 7].transform.position.x, 0.5f, board[4, 7].transform.position.z), Quaternion.identity);
-        Instantiate(blackYPrefab, new Vector3(board[3, 7].transform.position.x, 0.5f, board[3, 7].transform.position.z), Quaternion.identity);
-        Instantiate(blackPiPrefab, new Vector3(board[2, 7].transform.position.x, 0.5f, board[2, 7].transform.position.z), Quaternion.identity);
-        Instantiate(blackEpsilonPrefab, new Vector3(board[1, 7].transform.position.x, 0.5f, board[1, 7].transform.position.z), Quaternion.identity);
-        Instantiate(blackXPrefab, new Vector3(board[0, 7].transform.position.x, 0.5f, board[0, 7].transform.position.z), Quaternion.identity);
+        Instantiate(blackXPrefab, new Vector3(board[7, 7].transform.position.x, 0.5f, board[7, 7].transform.position.z), rotation);
+        Instantiate(blackDeltaPrefab, new Vector3(board[6, 7].transform.position.x, 0.5f, board[6, 7].transform.position.z), rotation);
+        Instantiate(blackPiPrefab, new Vector3(board[5, 7].transform.position.x, 0.5f, board[5, 7].transform.position.z), rotation);
+        Instantiate(blackSigmaPrefab, new Vector3(board[4, 7].transform.position.x, 0.5f, board[4, 7].transform.position.z), rotation);
+        Instantiate(blackYPrefab, new Vector3(board[3, 7].transform.position.x, 0.5f, board[3, 7].transform.position.z), rotation);
+        Instantiate(blackPiPrefab, new Vector3(board[2, 7].transform.position.x, 0.5f, board[2, 7].transform.position.z), rotation);
+        Instantiate(blackEpsilonPrefab, new Vector3(board[1, 7].transform.position.x, 0.5f, board[1, 7].transform.position.z), rotation);
+        Instantiate(blackXPrefab, new Vector3(board[0, 7].transform.position.x, 0.5f, board[0, 7].transform.position.z), rotation);
     }
 }
